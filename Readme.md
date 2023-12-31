8ac79d4c8301d5a4c609a75ce4e5d6894be0402a
akshaydhame2001 <akshaydhame2001@gmail.com>
2023-12-31 23:50:39 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+#React Essentials
+
+![img](React-essentials.jpeg)
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,4 @@
+export default function TabButton({ children, isSelected, ...props }) {
+    
+    return <li><button className={isSelected ? 'active' : undefined} {...props}>{children}</button></li>
+}
